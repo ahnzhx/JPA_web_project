3b302d4be2b727b2a3bcb7a7c979b10e4ca833d7
ahnzhx <modunaeggu@naver.com>
2020-04-01 08:37:39 +0900

    login and email check token

--- src/main/java/com/studyolle/account/AccountController.java
+++ src/main/java/com/studyolle/account/AccountController.java
@@ -11,17 +11,19 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
 
 @Controller
 @RequiredArgsConstructor
 public class AccountController {
 
 	private final SignUpFormValidator signUpFormValidator;
+	private final AccountService accountService;
 	private final AccountRepository accountRepository;
-	private final JavaMailSender javaMailSender;
 
 	@InitBinder("signUpForm")
 	public void initBinder(WebDataBinder webDataBinder){
@@ -40,25 +42,32 @@ public class AccountController {
 			return "account/sign-up";
 		}
 
-		Account account = Account.builder()
-				.email(signUpForm.getEmail())
-				.nickname(signUpForm.getNickname())
-				.password(signUpForm.getPassword()) // TODO encoding 해야함
-				.studyCreatedByWeb(true)
-				.studyEnrollmentResultByWeb(true)
-				.studyUpdatedByWeb(true)
-				.build();
+		accountService.processNewAccount(signUpForm);
 
-
-		Account newAccount = accountRepository.save(account);
-
-		newAccount.generateEmailCheckToken();
-		SimpleMailMessage mailMessage = new SimpleMailMessage();
-		mailMessage.setTo(newAccount.getEmail());
-		mailMessage.setSubject("스터디올래, 회원 가입 인증");
-		mailMessage.setText ("/check-email-token?token=" + newAccount.getEmailCheckToken()+
-					"&email="+newAccount.getEmail());
-		javaMailSender.send(mailMessage);
 		return "redirect:/";
 	}
+
+	@GetMapping("/check-email-token")
+	public String checkEmailToken(Model model, String token, String email){
+		Account account = accountRepository.findByEmail(email);
+		String view = "account/checked-email";
+		if(account == null){
+			model.addAttribute("error", "nonExisted.email");
+			return view;
+		}
+
+		if(!account.isValidToken(token)){
+			model.addAttribute("error", "different.token");
+			return view;
+		}
+
+		account.completeSignUp();
+
+		model.addAttribute("numberOfUser", accountRepository.count());
+		model.addAttribute("nickname", account.getNickname());
+
+		return view;
+	}
+
+
 }
