082158aa2ab6a109724f32420066bfbba744f999
ahnzhx <modunaeggu@naver.com>
2020-04-05 10:35:48 +0900

    completed login session

--- src/main/java/com/studyolle/account/AccountRepository.java
+++ src/main/java/com/studyolle/account/AccountRepository.java
@@ -13,5 +13,7 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     boolean existsByNickname(String nickname);
 
     Account findByEmail(String email);
+
+    Account findByNickname(String nickname);
 }
  
--- src/main/java/com/studyolle/config/SecurityConfig.java
+++ src/main/java/com/studyolle/config/SecurityConfig.java
@@ -1,16 +1,28 @@
 package com.studyolle.config;
 
+import com.studyolle.account.AccountService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	private final AccountService accountService;
+
+	private final DataSource dataSource;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -24,8 +36,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 				.loginPage("/login").permitAll();
 		http.logout()
 				.logoutSuccessUrl("/");
+		http.rememberMe()
+				.userDetailsService(accountService)
+				.tokenRepository(tokenRepository());
 	}
 
+	/**
+	쿠키 안전하게 설정
+	 토큰, 키값
+	 테이블에 저장해서 쓰기때문에 이에 맞는 디비 테이블과 필드를 만들어줘야 한다. (PersistentLogins.java)
+	 JdbcTokenRepositoryImpl 안에 "create~~ " 쿼리가 있는 것을 확인 가능
+	 */
+	@Bean
+	public PersistentTokenRepository tokenRepository(){
+		JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+		jdbcTokenRepository.setDataSource(dataSource);
+		return jdbcTokenRepository;
+	}
 	
 	@Override
 	public void configure(WebSecurity web) throws Exception {
